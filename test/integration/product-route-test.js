e972419c21416b00840c71d63a783d09163dcfef
J.Tadebois <jpm.tadebois@gmail.com>
2021-01-16 17:49:06 +0400

    Add integration test for POST /products

--- test/integration/product-route-test.js
+++ test/integration/product-route-test.js
@@ -3,12 +3,19 @@
 /* eslint-disable prefer-arrow-callback */
 
 const { before } = require('mocha');
-const { expect } = require('chai');
+const { expect, assert } = require('chai');
 const sinon = require('sinon');
 const request = require('supertest');
 const server = require('../../bin/www');
 
 describe('Product route integration test', async function () {
+  const product = {
+    id: 1,
+    name: 'iPhone 12 Pro',
+    description: 'The newest iPhone',
+    stock: 12,
+  };
+
   before(function (done) {
     if (server.listening) {
       server.close(done);
@@ -40,6 +47,72 @@ describe('Product route integration test', async function () {
         .then((res) => {
           const products = res.body;
           expect(products).to.nested.instanceOf(Array);
+        });
+    });
+  });
+
+  describe('POST product', function () {
+    it('returns 400 if name is missing', async function () {
+      await request(server)
+        .post('/products')
+        .send({
+          description: product.description,
+          stock: product.stock,
+        })
+        .expect(400);
+    });
+
+    it('returns 400 if stock is missing', async function () {
+      await request(server)
+        .post('/products')
+        .send({
+          name: product.name,
+          description: product.description,
+        })
+        .expect(400);
+    });
+
+    it('returns 200 when name and stock are not missing', async function () {
+      await request(server)
+        .post('/products')
+        .send({
+          name: product.name,
+          description: product.description,
+          stock: product.stock,
+        })
+        .expect(200);
+    });
+
+    it('returns the created product', function () {
+      return request(server)
+        .post('/products')
+        .send({
+          name: product.name,
+          description: product.description,
+          stock: product.stock,
+        })
+        .expect(200)
+        .then((res) => {
+          const createdProduct = res.body;
+          assert.deepEqual(createdProduct.name, product.name);
+          assert.deepEqual(createdProduct.description, product.description);
+          assert.deepEqual(createdProduct.stock, product.stock);
+        });
+    });
+
+    it('returns an id for the created product', function () {
+      return request(server)
+        .post('/products')
+        .send({
+          name: product.name,
+          description: product.description,
+          stock: product.stock,
+        })
+        .expect(200)
+        .then((res) => {
+          const createdProduct = res.body;
+          expect(createdProduct).to.haveOwnProperty('id');
+          assert.ok(createdProduct.id);
         });
     });
   });
