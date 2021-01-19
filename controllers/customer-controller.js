3a0979c0a9e605e75b97f0acfe493fd57c1293a0
J.Tadebois <jpm.tadebois@gmail.com>
2021-01-19 18:52:44 +0400

    Refactor validators to their own module

--- controllers/customer-controller.js
+++ controllers/customer-controller.js
@@ -10,34 +10,12 @@ module.exports = {
     }
   },
 
-  validateId(req, res, next, id) {
-    const customerId = Number(id);
-    if (customerId) {
-      req.body.id = customerId;
-      next();
-    } else {
-      res.status(400).send();
-    }
-  },
-
   async findByPk(req, res) {
     const customer = await CustomerRepo.findByPk(req.body.id);
     if (customer) {
       res.status(200).send(customer);
     } else {
       res.status(404).send();
-    }
-  },
-
-  validateCustomer(req, res, next) {
-    const { firstName } = req.body;
-    const { lastName } = req.body;
-    const { email } = req.body;
-
-    if (firstName && lastName && email) {
-      next();
-    } else {
-      res.status(400).send();
     }
   },
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ProductController = require('../controllers/product-controller.js');
 const CustomerController = require('../controllers/customer-controller.js');
+const Validators = require('../controllers/validators.js');
 
 const router = express.Router();
 
@@ -9,16 +10,16 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
-router.param('id', ProductController.validateId);
+router.param('id', Validators.validateId);
 router.get('/products', ProductController.findAll);
 router.get('/products/:id', ProductController.findByPk);
-router.post('/products', ProductController.validateProduct, ProductController.create);
-router.put('/products/:id', ProductController.validateProduct, ProductController.update);
+router.post('/products', Validators.validateProduct, ProductController.create);
+router.put('/products/:id', Validators.validateProduct, ProductController.update);
 router.delete('/products/:id', ProductController.delete);
 
-router.param('id', CustomerController.validateId);
+router.param('id', Validators.validateId);
 router.get('/customers', CustomerController.findAll);
 router.get('/customers/:id', CustomerController.findByPk);
-router.post('/customers', CustomerController.validateCustomer, CustomerController.create);
+router.post('/customers', Validators.validateCustomer, CustomerController.create);
 
 module.exports = router;
