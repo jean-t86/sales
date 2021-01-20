967e394f1bcc53cf0ef3ba7a1578ac6eec916473
J.Tadebois <jpm.tadebois@gmail.com>
2021-01-20 16:49:17 +0400

    Add order controller and /orders/* route

---
+++ controllers/order-controller.js
@@ -0,0 +1,58 @@
+const OrderRepo = require('../repos/order-repo.js');
+
+module.exports = {
+  async findAll(req, res) {
+    const orders = await OrderRepo.findAll();
+    if (orders) {
+      res.status(200).send(orders);
+    } else {
+      res.status(404).send();
+    }
+  },
+
+  async findByPk(req, res) {
+    const order = await OrderRepo.findByPk(req.body.id);
+    if (order) {
+      res.status(200).send(order);
+    } else {
+      res.status(404).send();
+    }
+  },
+
+  async create(req, res) {
+    const { customerId } = req.body;
+
+    const result = await OrderRepo.create(
+      customerId,
+    );
+
+    if (result) {
+      res.status(201).send(result);
+    } else {
+      res.status(400).send();
+    }
+  },
+
+  async update(req, res) {
+    const { customerId } = req.body;
+
+    const result = await OrderRepo.update(
+      req.body.id,
+      customerId,
+    );
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send();
+    }
+  },
+
+  async delete(req, res) {
+    const result = await OrderRepo.delete(req.body.id);
+    if (result) {
+      res.status(204).send();
+    } else {
+      res.status(404).send();
+    }
+  },
+};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ProductController = require('../controllers/product-controller.js');
 const CustomerController = require('../controllers/customer-controller.js');
+const OrderController = require('../controllers/order-controller.js');
 const Validators = require('../controllers/validators.js');
 
 const router = express.Router();
@@ -11,17 +12,23 @@ router.get('/', (req, res, next) => {
 });
 
 router.param('id', Validators.validateId);
+
 router.get('/products', ProductController.findAll);
 router.get('/products/:id', ProductController.findByPk);
 router.post('/products', Validators.validateProduct, ProductController.create);
 router.put('/products/:id', Validators.validateProduct, ProductController.update);
 router.delete('/products/:id', ProductController.delete);
 
-router.param('id', Validators.validateId);
 router.get('/customers', CustomerController.findAll);
 router.get('/customers/:id', CustomerController.findByPk);
 router.post('/customers', Validators.validateCustomer, CustomerController.create);
 router.put('/customers/:id', Validators.validateCustomer, CustomerController.update);
 router.delete('/customers/:id', Validators.validateId, CustomerController.delete);
+
+router.get('/orders', OrderController.findAll);
+router.get('/orders/:id', OrderController.findByPk);
+router.post('/orders', Validators.validateOrder, OrderController.create);
+router.put('/orders/:id', Validators.validateOrder, OrderController.update);
+router.delete('/orders/:id', Validators.validateId, OrderController.delete);
 
 module.exports = router;
