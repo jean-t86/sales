9e6f40b86580ba57a53312d54f029fd3a026b80c
J.Tadebois <jpm.tadebois@gmail.com>
2021-01-20 16:03:37 +0400

    Add sequelize Order model

---
+++ sequelize/models/order.js
@@ -0,0 +1,26 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Order extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Order.belongsTo(models.Customer);
+    }
+  }
+  Order.init({
+    customerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'Order',
+  });
+  return Order;
+};
---
+++ test/unit/sequelize/models/order-test.js
@@ -0,0 +1,25 @@
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable no-undef */
+
+const { assert, expect } = require('chai');
+const Sequelize = require('sequelize');
+const { Order } = require('../../../../sequelize/models');
+
+describe('Order sequelize model', function () {
+  it('has all the correct properties', function () {
+    expect(Order.tableAttributes).to.have.ownProperty('customerId');
+  });
+
+  it('has the correct constraint on id field', function () {
+    assert.deepEqual(Order.tableAttributes.id.type, Sequelize.DataTypes.INTEGER());
+    expect(Order.tableAttributes.id.allowNull).to.equal(false);
+    assert.ok(Order.tableAttributes.id.primaryKey);
+    assert.ok(Order.tableAttributes.id.autoIncrement);
+  });
+
+  it('has the correct constraints on customerId field', function () {
+    assert.deepEqual(Order.tableAttributes.customerId.type, Sequelize.DataTypes.INTEGER());
+    expect(Order.tableAttributes.customerId.allowNull).to.equal(false);
+  });
+});
